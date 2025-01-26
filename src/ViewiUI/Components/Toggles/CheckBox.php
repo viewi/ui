ba983a7de6477bfce631766e305ec2d2df67f92e
Ivan Voitovych <col606v@gmail.com>
2025-01-26 12:57:36 +0200

    CheckBox

---
+++ src/ViewiUI/Components/Toggles/CheckBox.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Viewi\UI\Components\Toggles;
+
+use Viewi\Components\BaseComponent;
+use Viewi\Components\DOM\DomEvent;
+use Viewi\Components\DOM\HtmlNode;
+use Viewi\DI\Inject;
+use Viewi\DI\Scope;
+use Viewi\UI\Components\Forms\FormContext;
+use Viewi\UI\Components\Validation\ValidationMessage;
+
+class CheckBox extends BaseComponent
+{
+    public ?string $label = null;
+    public ?string $id = null;
+    public ?string $name = null;
+    public ?string $model = null;
+    public ?string $hint = null;
+    public ?HtmlNode $input = null;
+    private ?ValidationMessage $validationMessages = null;
+    public $isInvalid = false;
+
+    public function __construct(
+        #[Inject(Scope::PARENT)]
+        private FormContext $form
+    ) {}
+
+    public function mounted()
+    {
+        $this->form->inputs[$this->id ?? $this->name ?? $this->__id] = function ($valid, $errors) {
+            $this->isInvalid = !$valid;
+            $this->validationMessages->show = $this->isInvalid;
+            $this->validationMessages->messages = $errors;
+        };
+    }
+
+    public function onChange(DomEvent $event)
+    {
+        $this->validationMessages->show = false;
+        $this->isInvalid = false;
+        $this->emitEvent('model', $event->target->checked);
+    }
+}
