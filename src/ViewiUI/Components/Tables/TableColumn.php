23769d819a7ffe4de871495d9e28ceef9eb2ba07
Ivan Voitovych <col606v@gmail.com>
2025-02-03 10:02:11 +0200

    DataTable: column template

--- src/ViewiUI/Components/Tables/TableColumn.php
+++ src/ViewiUI/Components/Tables/TableColumn.php
@@ -4,8 +4,5 @@ namespace Viewi\UI\Components\Tables;
 
 class TableColumn
 {
-    public function __construct(public string $key, public ?string $type = null, public ?string $title = null) {}
-
-    const TYPE_DATETIME = 'datetime';
-    const TYPE_BOOLEAN = 'boolean';
+    public function __construct(public string $key, public ?string $title = null, public ?string $template = null) {}
 }
