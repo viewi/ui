a8e21d36070de1cbc7ce502a1d89d4a945ed4a87
Ivan Voitovych <col606v@gmail.com>
2025-02-11 11:49:39 +0200

    FormContext

--- src/ViewiUI/Components/Forms/ActionForm.php
+++ src/ViewiUI/Components/Forms/ActionForm.php
@@ -14,18 +14,14 @@ class ActionForm extends BaseComponent
     public ?string $classList = null;
     public ?string $action = null;
     public ?string $autocomplete = null;
-    public FormContext $formContext;
     public array $rules = [];
 
     public function __construct(
         #[Inject(Scope::COMPONENT)]
         private FormContext $form
-    ) {
-    }
+    ) {}
 
-    public function init()
-    {
-    }
+    public function init() {}
 
     public function handleSubmit(DomEvent $event)
     {
@@ -34,46 +30,6 @@ class ActionForm extends BaseComponent
 
     public function validate(): bool
     {
-        $finalValid = true;
-        $generalMessages = [];
-        foreach ($this->rules as $field => $rules) {
-            $messages = [];
-            foreach ($rules as $_ => $action) {
-                $validationResult = $action();
-                $valid = $validationResult === true;
-                $finalValid = $finalValid && $valid;
-                if (!$valid) {
-                    $messages[] = $validationResult || 'Validation has failed';
-                }
-            }
-            /**
-             * @var callback
-             */
-            $inputCallback = $this->form->inputs[$field] ?? false;
-            if (count($messages) > 0) {
-                if ($inputCallback) {
-                    $inputCallback(false, $messages);
-                } else {
-                    $generalMessages = array_merge($generalMessages, $messages);
-                }
-            } else {
-                if ($inputCallback) {
-                    $inputCallback(true, $messages);
-                }
-            }
-        }
-        /**
-         * @var callback
-         */
-        $fallbackMessagesCallback = $this->form->inputs['fallbackMessages'] ?? false;
-
-        if ($fallbackMessagesCallback) {
-            if ($generalMessages) {
-                $fallbackMessagesCallback(false, $generalMessages);
-            } else {
-                $fallbackMessagesCallback(true, $generalMessages);
-            }
-        }
-        return $finalValid; // $valid;
+        return $this->form->validate($this->rules);
     }
 }
--- src/ViewiUI/Components/Forms/FormContext.php
+++ src/ViewiUI/Components/Forms/FormContext.php
@@ -5,4 +5,49 @@ namespace Viewi\UI\Components\Forms;
 class FormContext
 {
     public array $inputs = [];
+
+    public function validate(array $ruleList): bool
+    {
+        $finalValid = true;
+        $generalMessages = [];
+        foreach ($ruleList as $field => $rules) {
+            $messages = [];
+            foreach ($rules as $_ => $action) {
+                $validationResult = $action();
+                $valid = $validationResult === true;
+                $finalValid = $finalValid && $valid;
+                if (!$valid) {
+                    $messages[] = $validationResult || 'Validation has failed';
+                }
+            }
+            /**
+             * @var callback
+             */
+            $inputCallback = $this->inputs[$field] ?? false;
+            if (count($messages) > 0) {
+                if ($inputCallback) {
+                    $inputCallback(false, $messages);
+                } else {
+                    $generalMessages = array_merge($generalMessages, $messages);
+                }
+            } else {
+                if ($inputCallback) {
+                    $inputCallback(true, $messages);
+                }
+            }
+        }
+        /**
+         * @var callback
+         */
+        $fallbackMessagesCallback = $this->inputs['fallbackMessages'] ?? false;
+
+        if ($fallbackMessagesCallback) {
+            if ($generalMessages) {
+                $fallbackMessagesCallback(false, $generalMessages);
+            } else {
+                $fallbackMessagesCallback(true, $generalMessages);
+            }
+        }
+        return $finalValid; // $valid;
+    }
 }
