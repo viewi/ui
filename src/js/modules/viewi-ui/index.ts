b2648364ec28e0fc48e2923868c9476fb95c8b72
Ivan Voitovych <col606v@gmail.com>
2025-02-15 17:57:30 +0200

    index.ts

--- src/js/modules/viewi-ui/index.ts
+++ src/js/modules/viewi-ui/index.ts
@@ -1,6 +1,8 @@
 import { ExpandTransition } from "./transition/ExpandTransition";
 import { CKEditor } from "./editors/CKEditor";
+import { CodeEditor } from "./editors/CodeEditor";
 export {
     ExpandTransition,
-    CKEditor
+    CKEditor,
+    CodeEditor
 };
